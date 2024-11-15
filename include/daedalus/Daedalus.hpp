3503a15c2cc0f0394e484cd6f1c680502c4c50af
Martin Larsson <larssonmartin1998@gmail.com>
2024-11-15 01:32:25 +0100

    Integrate start/shutdown in Daedalus

--- include/daedalus/Daedalus.hpp
+++ include/daedalus/Daedalus.hpp
@@ -5,6 +5,9 @@
 namespace daedalus {
 class Daedalus final : public IDaedalus {
   public:
+    auto start() -> void override;
+    auto shutdown() -> void override;
+
     [[nodiscard]] auto should_quit() const -> bool override;
 };
 } // namespace daedalus
--- src/daedalus/Daedalus.cpp
+++ src/daedalus/Daedalus.cpp
@@ -1,5 +1,10 @@
 #include "Daedalus.hpp"
 
+#include <print>
+
 namespace daedalus {
+auto Daedalus::start() -> void { std::println("Daedalus::start()"); }
+auto Daedalus::shutdown() -> void { std::println("Daedalus::shutdown()"); }
+
 auto Daedalus::should_quit() const -> bool { return true; }
 } // namespace daedalus
