c95f26d4447f5e8ab3f689a630e881b0000dccff
Martin Lasson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-02-21 01:29:21 +0100

    Add support for aarch64/x86_64 darwin/linux

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     atlas.url = "github:LarssonMartin1998/atlas";
   };
 
@@ -10,44 +11,47 @@
     {
       self,
       nixpkgs,
+      flake-utils,
       atlas,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      stdenv = pkgs.llvmPackages.stdenv;
-    in
-    {
-      packages.${system}.default = stdenv.mkDerivation {
-        pname = "atlas";
-        version = "0.1.0";
-        src = ./.;
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        stdenv = pkgs.llvmPackages.stdenv;
+      in
+      {
+        packages.default = stdenv.mkDerivation {
+          pname = "atlas";
+          version = "0.1.0";
+          src = ./.;
 
-        nativeBuildInputs = with pkgs; [
-          clang-tools
-          clang
-          ninja
-          cmake
-        ];
+          nativeBuildInputs = with pkgs; [
+            clang-tools
+            clang
+            ninja
+            cmake
+          ];
 
-        cmakeFlags = [
-          "-DCMAKE_BUILD_TYPE=Release"
-          "-DBUILD_TESTS=ON"
-          "-DENABLE_INSTALL=ON"
-        ];
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+            "-DBUILD_TESTS=ON"
+            "-DENABLE_INSTALL=ON"
+          ];
 
-        # Were depending on running `add_subdirectory(atlas)` in the CMakeLists.txt
-        # This has a lot of benefits when it comes to the modularity of the project, but it
-        # also hinders us from just adding the library as an flake- and buildInput.
-        preConfigure = ''
-          cp -r ${atlas.src} $(pwd)/atlas
-          chmod -R u+w atlas
-        '';
+          # Were depending on running `add_subdirectory(atlas)` in the CMakeLists.txt
+          # This has a lot of benefits when it comes to the modularity of the project, but it
+          # also hinders us from just adding the library as an flake- and buildInput.
+          preConfigure = ''
+            cp -r ${atlas.src} $(pwd)/atlas
+            chmod -R u+w atlas
+          '';
 
-        doCheck = true;
-        checkPhase = ''
-          ctest --output-on-failure
-        '';
-      };
-    };
+          doCheck = true;
+          checkPhase = ''
+            ctest --output-on-failure
+          '';
+        };
+      }
+    );
 }
