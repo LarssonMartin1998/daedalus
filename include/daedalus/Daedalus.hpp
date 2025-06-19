0ed3b4defd436453044bb5f67b82b8889795b968
Martin Larsson <larssonmartin1998@gmail.com>
2025-06-19 14:33:36 +0200

    Update daedalus.h/c-pp to work with latest changes from heph-perf in atlas

--- include/daedalus/Daedalus.hpp
+++ include/daedalus/Daedalus.hpp
@@ -6,8 +6,13 @@
 namespace daedalus {
 class Daedalus final : public atlas::core::Game, public IDaedalus {
   public:
+    auto pre_start() -> void override {}
     auto start() -> void override;
+    auto post_start() -> void override {}
+
+    auto pre_shutdown() -> void override {}
     auto shutdown() -> void override;
+    auto post_shutdown() -> void override {}
 
     [[nodiscard]] auto should_quit() const -> bool override;
 };
--- src/daedalus/Daedalus.cpp
+++ src/daedalus/Daedalus.cpp
@@ -1,77 +1,106 @@
 #include "Daedalus.hpp"
 
 #include "core/IEngine.hpp"
+#include "core/time/IEngineClock.hpp"
 
+#include "hephaestus/Component.hpp"
 #include "hephaestus/Hephaestus.hpp"
-#include "hephaestus/IComponent.hpp"
-#include "hephaestus/Query.hpp"
 #include <print>
+#include <tuple>
 
 using namespace atlas;
-using hephaestus::IComponent;
+using hephaestus::Component;
 
-struct Transform : public IComponent {
+struct Transform : Component<Transform> {
     int x{}, y{}, z{};
 };
 
-struct Velocity : public IComponent {
+struct Velocity : Component<Velocity> {
     int x{}, y{}, z{};
 };
 
-struct Health : public IComponent {
+struct Health : Component<Health> {
     int health{};
 };
 
 namespace daedalus {
 auto Daedalus::start() -> void {
     auto& hephaestus = get_engine().get_module<hephaestus::Hephaestus>().get();
-    hephaestus.create_entity(Velocity{},
-                             Transform{
-                                 .x = 10000000,
-                                 .y = -9955123,
-                             },
-                             Health{.health = 100});
+    constexpr size_t num_ents = 100000;
+    for (int i = 0; i < num_ents; ++i) {
+        hephaestus.create_entity(Transform{.x = i, .y = i, .z = i});
+    }
+    for (int i = 0; i < num_ents; ++i) {
+        hephaestus.create_entity(Transform{.x = i, .y = i, .z = i},
+                                 Velocity{.x = i, .y = i, .z = i});
+    }
+    constexpr int health = 5000;
+    for (int i = 0; i < num_ents; ++i) {
+        hephaestus.create_entity(Transform{.x = i, .y = i, .z = i},
+                                 Velocity{.x = i, .y = i, .z = i},
+                                 Health{.health = health});
+    }
 
-    hephaestus.create_entity(Health{.health = 5000});
-
-    hephaestus.create_system(
-        [](const core::IEngine& engine,
-           const hephaestus::Query<Transform, Velocity>& query) {
-            for (const auto& [transform, velocity] : query.get()) {
-                transform.x += velocity.x;
-                transform.y += velocity.y;
-                transform.z += velocity.z;
-            }
-        });
+    hephaestus.create_entity(Health{.health = health});
 
     hephaestus.create_system([](const core::IEngine& engine,
-                                const hephaestus::Query<Velocity>& query) {
-        for (const auto& [velocity] : query.get()) {
+                                std::tuple<Transform&, Velocity&> data) {
+        auto& [transform, velocity] = data;
+        transform.x += velocity.x;
+        transform.y += velocity.y;
+        transform.z += velocity.z;
+    });
+
+    hephaestus.create_system(
+        [](const core::IEngine& engine, std::tuple<Velocity&> data) {
+            auto& [velocity] = data;
             velocity.x = 1;
             velocity.y = 1;
             velocity.z = 1;
-        }
-    });
+        });
 
-    hephaestus.create_system([](const core::IEngine& engine,
-                                const hephaestus::Query<Transform>& query) {
-        for (const auto& [transform] : query.get()) {
+    hephaestus.create_system(
+        [](const core::IEngine& engine, std::tuple<Transform&> data) {
+            auto& [transform] = data;
             std::println("Transform: x={}, y={}, z={}", transform.x,
                          transform.y, transform.z);
-        }
-    });
+        });
 
-    hephaestus.create_system([](const core::IEngine& engine,
-                                const hephaestus::Query<Health>& query) {
-        for (const auto& [health] : query.get()) {
+    hephaestus.create_system(
+        [](const core::IEngine& engine, std::tuple<Health&> data) {
+            auto& [health] = data;
             std::println("Health: {}", health.health);
-        }
-    });
+        });
+
+    hephaestus.create_system(
+        [](const core::IEngine& engine, std::tuple<Velocity&> data) {
+            auto& [velocity] = data;
+            velocity.x = 1;
+            velocity.y = 1;
+            velocity.z = 1;
+        });
+
+    hephaestus.create_system(
+        [](const core::IEngine& engine, std::tuple<Transform&> data) {
+            auto& [transform] = data;
+            std::println("Transform: x={}, y={}, z={}", transform.x,
+                         transform.y, transform.z);
+        });
+
+    hephaestus.create_system(
+        [](const core::IEngine& engine, std::tuple<Health&> data) {
+            auto& [health] = data;
+            std::println("Health: {}", health.health);
+        });
 
     std::println("Daedalus::start()");
 }
 
 auto Daedalus::shutdown() -> void { std::println("Daedalus::shutdown()"); }
 
-auto Daedalus::should_quit() const -> bool { return false; }
+auto Daedalus::should_quit() const -> bool {
+    constexpr float game_lifetime = 5.F;
+    return get_engine().get_clock().get_total_time() >= game_lifetime;
+    // return false;
+}
 } // namespace daedalus
